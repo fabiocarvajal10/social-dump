f9ff33a4ec19cb12fe0fb30cc154599ac4ed4b4a
= <franm29@outlook.com>
2015-07-10 11:45:07 -0600

    us-2 estandares

--- src/main/java/com/coredump/socialdump/web/rest/UserXAuthTokenController.java
+++ src/main/java/com/coredump/socialdump/web/rest/UserXAuthTokenController.java
@@ -35,12 +35,17 @@ public class UserXAuthTokenController {
     @RequestMapping(value = "/authenticate",
             method = RequestMethod.POST)
     @Timed
-    public Token authorize(@RequestParam String username, @RequestParam String password) {
+    public Token authorize(@RequestParam String username,
+                           @RequestParam String password) {
 
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
-        Authentication authentication = this.authenticationManager.authenticate(token);
+        UsernamePasswordAuthenticationToken token =
+            new UsernamePasswordAuthenticationToken(username, password);
+        Authentication authentication =
+            this.authenticationManager.authenticate(token);
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        UserDetails details = this.userDetailsService.loadUserByUsername(username);
+        UserDetails details =
+            this.userDetailsService.loadUserByUsername(username);
+
         return tokenProvider.createToken(details);
     }
 }
