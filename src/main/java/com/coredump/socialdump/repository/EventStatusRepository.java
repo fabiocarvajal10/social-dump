ca8b85a82f14bd4a025099a0ec83432cd412a249
fabio <fabiocarvajal10@gmail.com>
2015-07-13 04:14:00 -0600

    Working on events CRUD

---
+++ src/main/java/com/coredump/socialdump/repository/EventStatusRepository.java
@@ -0,0 +1,7 @@
+package com.coredump.socialdump.repository;
+
+/**
+ * Created by fabio on 13/07/15.
+ */
+public interface EventStatusRepository {
+}
---
+++ src/main/java/com/coredump/socialdump/web/rest/dto/EventDTO.java
@@ -0,0 +1,7 @@
+package com.coredump.socialdump.web.rest.dto;
+
+/**
+ * Created by fabio on 13/07/15.
+ */
+public class EventDTO {
+}
