d0ace0f4cbaeb0a078f7b68d35b31226d8426aa2
= <franm29@outlook.com>
2015-07-18 06:21:38 -0600

    organizaciones, nav, sesion

--- src/main/java/com/coredump/socialdump/service/InstagramFetch.java
+++ src/main/java/com/coredump/socialdump/service/InstagramFetch.java
@@ -1,16 +1,11 @@
 package com.coredump.socialdump.service;
 
-import com.coredump.socialdump.domain.SocialNetwork;
 import com.coredump.socialdump.domain.SocialNetworkPost;
 
 import org.jinstagram.Instagram;
-import org.jinstagram.auth.InstagramAuthService;
 import org.jinstagram.auth.model.Token;
-import org.jinstagram.auth.model.Verifier;
 import org.jinstagram.auth.oauth.InstagramService;
 import org.jinstagram.entity.tags.TagMediaFeed;
-import org.jinstagram.entity.users.basicinfo.UserInfo;
-import org.jinstagram.entity.users.feed.MediaFeed;
 import org.jinstagram.entity.users.feed.MediaFeedData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,14 +28,14 @@ import javax.print.DocFlavor;
 @Scope("prototype")
 public class InstagramFetch extends SocialNetworkFetch {
 
-  private final String ACCESS_TOKEN = "1339991245.964ebd3.d5494e068a314378a11a6c10e3860e99";
+  private static final String ACCESS_TOKEN = "1339991245.964ebd3.d5494e068a314378a11a6c10e3860e99";
 
   private final Logger log = LoggerFactory.getLogger(InstagramFetch.class);
 
   private Instagram instagram;
-    private InstagramService instagramService;
+  private InstagramService instagramService;
 
-    public InstagramFetch() {
+  public InstagramFetch() {
     super();
   }
 
@@ -50,28 +45,34 @@ public class InstagramFetch extends SocialNetworkFetch {
         new Token(ACCESS_TOKEN, getSocialNetworkApiCredential().getAppSecret());
     instagram = new Instagram(accessToken);
     List<SocialNetworkPost> postsList = new ArrayList<>();
-      while (true) {
-          try {
-              log.debug("Obteniendo grams de: {}...", getSearchCriteria().getSearchCriteria());
-              TagMediaFeed mediaFeed =
-                  instagram.getRecentMediaTags(getSearchCriteria().getSearchCriteria());
-              List<MediaFeedData> mediaFeeds = mediaFeed.getData();
+    while (true) {
+      try {
+        log.debug("Obteniendo grams de: {}...", getSearchCriteria().getSearchCriteria());
+        TagMediaFeed mediaFeed =
+            instagram.getRecentMediaTags(getSearchCriteria().getSearchCriteria());
+        List<MediaFeedData> mediaFeeds = mediaFeed.getData();
 
-              log.debug("Cantidad de posts (inst) obtenidos: {}...", mediaFeeds.size());
-              for (int i = 0; i < mediaFeeds.size(); i++) {
-                  postsList.add(processGram(mediaFeeds.get(i)));
-              }
+        log.debug("Cantidad de posts (inst) obtenidos: {}...", mediaFeeds.size());
+        mediaFeeds.forEach(post -> postsList.add(processGram(post)));
 
-              //log.debug("Guardando los grams obtenidos");
-              getSocialNetworkPostRepository().save(postsList);
-              postsList.clear();
-              log.debug("Sleeping");
-              Thread.sleep(10000);
+        log.debug("Guardando los grams obtenidos");
+        getSocialNetworkPostRepository().save(postsList);
+        postsList.clear();
+        log.debug("Sleeping");
+        Thread.sleep(10000);
 
-          } catch (Exception e) {
-              e.printStackTrace();
-          }
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      } catch (Exception e) {
+        e.printStackTrace();
+        try {
+          Thread.sleep(30000);
+        } catch (InterruptedException ex) {
+          log.debug("Exception putting thread to sleep");
+          ex.printStackTrace();
+        }
       }
+    }
   }
 
   private SocialNetworkPost processGram(MediaFeedData mediaFeedData) {
@@ -79,7 +80,7 @@ public class InstagramFetch extends SocialNetworkFetch {
 
     post.setBody(mediaFeedData.getCaption().getText().replaceAll("[^\\x20-\\x7e]", ""));
     post.setCreatedAt(new Timestamp(new Date().getTime()));
-    post.setSnUserId(new Long(1));
+    post.setSnUserId(Long.parseLong(mediaFeedData.getUser().getId()));
     post.setMediaUrl(mediaFeedData.getLink());
     post.setSnUserEmail(mediaFeedData.getUser().getUserName());
     post.setEventByEventId(getSearchCriteria().getEventByEventId());
--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,9 +1,16 @@
 'use strict';
 
 angular.module('socialdumpApp')
-  .controller('MainController', function($scope, Principal) {
+  .controller('MainController', function($scope, Principal, AuthServerProvider, $state) {
     Principal.identity().then(function(account) {
       $scope.account = account;
       $scope.isAuthenticated = Principal.isAuthenticated;
+    });
+
+    $scope.$watch('isAuthenticated', function(){
+      if(!Principal.isAuthenticated() || !AuthServerProvider.hasValidToken()){
+        AuthServerProvider.logout();
+        $state.go('login');
+      }
     });
   });
