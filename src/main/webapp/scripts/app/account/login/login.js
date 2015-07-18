4e7582660128ff554c99fc4525eaf9ac3cef9728
Esteban <joseestetq@hotmail.com>
2015-07-17 23:19:11 -0600

    Fix login/register/docs

--- src/main/webapp/scripts/app/account/login/login.js
+++ src/main/webapp/scripts/app/account/login/login.js
@@ -1,16 +1,23 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('socialdumpApp')
-  .config(function($stateProvider) {
-    $stateProvider
-        .state('login', {
-          url: '/login',
-          data: {
-            roles: [],
-            pageTitle: 'Iniciar Sesión'
-          },
-          templateUrl: 'scripts/app/account/login/login.html',
-          controller: 'LoginController',
-          resolve: {}
-        });
-  });
+  angular.module('socialdumpApp')
+    .config(function($stateProvider) {
+      $stateProvider
+          .state('login', {
+            parent: 'account',
+            url: '/login',
+            data: {
+              roles: [],
+              pageTitle: 'Iniciar Sesión'
+            },
+            views: {
+              'content@': {
+                templateUrl: 'scripts/app/account/login/login.html',
+                controller: 'LoginController'
+              }
+            },
+            resolve: {}
+          });
+    });
+}());
--- src/main/webapp/scripts/app/account/register/register.js
+++ src/main/webapp/scripts/app/account/register/register.js
@@ -1,15 +1,24 @@
-'use strict';
+(function() {
 
-angular.module('socialdumpApp')
-  .config(function($stateProvider) {
-    $stateProvider
-      .state('register', {
-        url: '/register',
-        data: {
-          roles: [],
-          pageTitle: 'Crear Cuenta'
-        },
-        templateUrl: 'scripts/app/account/register/register.html',
-        controller: 'RegisterController'
+  'use strict';
+
+  angular.module('socialdumpApp')
+    .config(function($stateProvider) {
+      $stateProvider
+        .state('register', {
+          parent: 'account',
+          url: '/register',
+          data: {
+            roles: [],
+            pageTitle: 'Crear Cuenta'
+          },
+          views: {
+            'content@': {
+              templateUrl: 'scripts/app/account/register/register.html',
+              controller: 'RegisterController'
+            }
+          },
+          resolve: {}
+      });
     });
-  });
+}());
