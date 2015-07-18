f34a221e4bc26e6bcdeb98425dfd4575765c482b
Esteban <joseestetq@hotmail.com>
2015-07-18 07:03:39 -0600

    Incorporate changes to Org Service

--- src/main/webapp/scripts/components/organization/OrganizationService.js
+++ src/main/webapp/scripts/components/organization/OrganizationService.js
@@ -1,21 +1,21 @@
 /**
  * Created by Franz on 17/07/2015.
  */
-(function() {
-  'use strict';
+'use strict';
 
-  angular.module('socialdumpApp')
-    .factory(
-      'OrganizationService', [
-        '$http', '$q', 'localStorageService', '$location',
-        function($http, $q, localStorageService, $location) {
-          var rootUrl = $location.protocol() + '://' + $location.host() + ':' +
-                        $location.port();
-          var orgsUrl = rootUrl + '/api/organizations';
-          var currOrgId = 1;
+angular.module('socialdumpApp')
+  .factory('OrganizationService', function ($http, $q, localStorageService) {
+    var rootUrl = $location.protocol() + '://' + $location.host() + ':' +
+                  $location.port();
+    var orgsUrl = rootUrl + '/api/organizations';
+    var currOrgId = 1;
+    return {
+      register: function(organizationName) {
+        var organization = {
+          'name': organizationName,
+          'ownerId': parseInt(localStorageService.get('userId'))
+        }
 
-<<<<<<< HEAD
-=======
         var q = $q.defer();
         $http({
           url: 'http://127.0.0.1:9090/api/organizations',
@@ -35,30 +35,10 @@
           }
           q.reject(err);
         });
->>>>>>> development
 
-          return {
-            register: function(organizationName) {
-              var organization = {
-                'name': organizationName,
-                'ownerId': parseInt(localStorageService.get('userId'))
-              };
+        return q.promise;
+      },
 
-<<<<<<< HEAD
-              var q = $q.defer();
-              $http({
-                url: 'http://127.0.0.1:9090/api/organizations',
-                method: 'POST',
-                data: organization
-              }).
-              success(function(data) {
-                q.resolve(data);
-              }).
-              error(function(error) {
-                console.log(error);
-                q.reject(error);
-              });
-=======
       getAll: function() {
         var q = $q.defer();
         $http({
@@ -116,25 +96,10 @@
           }
           q.reject(err);
         });
->>>>>>> development
 
-              return q.promise;
-            },
+        return q.promise;
+      },
 
-<<<<<<< HEAD
-            getAll: function() {
-              var q = $q.defer();
-              $http({
-                url: 'http://127.0.0.1:9090/api/organizations',
-                method: 'GET'
-              }).
-              success(function(data) {
-                q.resolve(data);
-              }).
-              error(function(error) {
-                 q.reject(error);
-              });
-=======
       delete: function(id) {
         var q = $q.defer();
         $http({
@@ -142,45 +107,25 @@
           method: 'DELETE',
           data: id
         }).
-        success(function(data){
+        success(function(data) {
           q.resolve(data);
         }).
-        catch(function(error) {
+        catch (function(error) {
           var err = error.data.exception;
-          if(err === 'org.springframework.dao.DataIntegrityViolationException') {
+          if (err === 'org.springframework.dao.DataIntegrityViolationException') {
             err = 'No se puede eliminar la organización. La organización cuenta con eventos';
           }else {
             err = 'Error inesperado al intentar eliminar la organización';
           }
           q.reject(err);
         });
->>>>>>> development
 
-              return q.promise;
-            },
+        return q.promise;
+      },
 
-            delete: function(id) {
-              alert(localStorageService.get('userId'));
-              var q = $q.defer();
-              $http({
-                url: 'http://127.0.0.1:9090/api/organizations/' + id,
-                method: 'DELETE',
-                data: id
-              }).
-              success(function(data) {
-                q.resolve(data);
-              }).
-              catch (function(error) {
-                console.log(error);
-                q.reject(error);
-              });
+      getCurrentOrgId: function() {
+       return currOrgId;
+      }
 
-              return q.promise;
-            },
-
-             getCurrentOrgId: function() {
-              return currOrgId;
-             }
-           };
-         }]);
-}());
+     };
+   });
