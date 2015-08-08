95e6138e2b8e98529532de47514ab00cc1dc749e
= <franm29@outlook.com>
2015-08-08 00:33:52 -0600

    modificar cuenta

--- src/main/webapp/scripts/app/account/settings/settings.controller.js
+++ src/main/webapp/scripts/app/account/settings/settings.controller.js
@@ -1,23 +1,30 @@
 'use strict';
 
 angular.module('socialdumpApp')
-    .controller('SettingsController', function ($scope, Principal, Auth) {
-        $scope.success = null;
+  .controller('SettingsController', ['$scope', 'Principal', 'Auth',
+      function ($scope, Principal, Auth) {
+    $scope.success = null;
+    $scope.error = null;
+    Principal.identity(true).then(function(account) {
+      $scope.settingsAccount = account;
+    });
+
+    $scope.save = function () {
+      Auth.updateAccount($scope.settingsAccount).then(function() {
         $scope.error = null;
-        Principal.identity(true).then(function(account) {
+        $scope.errorEmailExists = false;
+        $scope.success = 'OK';
+        Principal.identity().then(function(account) {
             $scope.settingsAccount = account;
         });
-
-        $scope.save = function () {
-            Auth.updateAccount($scope.settingsAccount).then(function() {
-                $scope.error = null;
-                $scope.success = 'OK';
-                Principal.identity().then(function(account) {
-                    $scope.settingsAccount = account;
-                });
-            }).catch(function() {
-                $scope.success = null;
-                $scope.error = 'ERROR';
-            });
-        };
-    });
+      }).catch(function(error) {
+        if(error.data.exception
+           === 'org.springframework.dao.DataIntegrityViolationException') {
+          $scope.errorEmailExists = true;
+        } else {
+          $scope.success = null;
+          $scope.error = 'ERROR';
+        }
+      });
+    };
+  }]);
--- src/main/webapp/scripts/app/account/settings/settings.js
+++ src/main/webapp/scripts/app/account/settings/settings.js
@@ -4,20 +4,16 @@ angular.module('socialdumpApp')
     .config(function ($stateProvider) {
         $stateProvider
             .state('settings', {
-                parent: 'account',
-                url: '/settings',
+                parent: 'home',
+                url: 'settings',
                 data: {
                     roles: ['ROLE_USER'],
                     pageTitle: 'Settings'
                 },
-                views: {
-                    'content@': {
-                        templateUrl: 'scripts/app/account/settings/settings.html',
-                        controller: 'SettingsController'
-                    }
-                },
+                templateUrl: 'scripts/app/account/settings/settings.html',
+                controller: 'SettingsController',
                 resolve: {
-                    
+
                 }
             });
     });
