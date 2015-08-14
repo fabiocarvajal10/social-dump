431382f6a7da5e8f81390ba5a94764aa7e5c9ea5
= <franm29@outlook.com>
2015-08-13 23:56:45 -0600

    interceptors

---
+++ src/main/webapp/scripts/components/interceptors/util.interceptor.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Franz on 13/08/2015.
+ */
+angular.module('socialdumpApp')
+  .config(['$httpProvider', function($httpProvider){
+    $httpProvider.interceptors.push(function($q, $injector){
+      return {
+        'responseError': function(rejection) {
+          var modal = $injector.get('UtilInterceptorService');
+          var status = rejection.status;
+          var createMessage = function(title, body) {
+            return {
+              'title': title,
+              'body': body
+            }
+          }
+
+          switch(status) {
+            case 0:
+                  modal.openNoInternet();
+                  break;
+            case 500:
+                  modal.open(createMessage('Error al completar su solicitud',
+                    'Algo ha salido mal. En este momento no se puede completar su solicitud. ' +
+                    'Por favor inténtelo después'));
+                  break;
+            default:
+                  break;
+          }
+
+          return $q.reject(rejection);
+        }
+      };
+    });
+  }]);
