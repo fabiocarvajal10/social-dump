f23926b56e51bec5a9759298a10aa0d6f435b55a
= <franm29@outlook.com>
2015-08-17 10:07:58 -0600

    quitando codigo inecesario

--- src/main/webapp/scripts/components/entities/monitor/owner.service.js
+++ src/main/webapp/scripts/components/entities/monitor/owner.service.js
@@ -9,9 +9,7 @@
       function($rootScope, $q, Event, $state) {
         return {
           'validate': function(eventId){
-            console.log('crap');
             if ($rootScope.tempAccessId === undefined || $rootScope.tempAccessId === null) {
-              console.log('damn');
               Event.validateOwnership({id:eventId})
                 .$promise.then(function(data) {
 
